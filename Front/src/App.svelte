ae8dd610f6a00879acf7c6cb923e9d0999d2b812
Andréas ade <andrecot2502@gmail.com>
2023-03-27 10:38:36 +0200

    Test

--- Front/src/App.svelte
+++ Front/src/App.svelte
@@ -4,9 +4,14 @@
   import TestApi from './components/TestApiComponents/TestApi.svelte';
   import CreateAccountForm from './components/CreateAccount.svelte';
   import LoginForm from './components/LoginForm.svelte';
+  import Header from './components/Header.svelte';
 </script>
 
 <main>
+  <header>
+    <Header />
+  </header>
+  
   <p class="text-3xl font-bold">
     Test
   </p>
---
+++ Front/src/components/Header.svelte
@@ -0,0 +1,3 @@
+<div class="bg-red-500 w-full">
+    <h1>Test</h1>
+</div>
