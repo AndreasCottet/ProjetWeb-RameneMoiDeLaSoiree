9bf4ce9abb607f231736fdac96721394dcbdb46c
Andréas ade <andrecot2502@gmail.com>
2023-03-29 15:35:19 +0200

    WIP

--- Front/src/App.svelte
+++ Front/src/App.svelte
@@ -7,11 +7,16 @@
   import Header from './components/Header.svelte';
   import CaseTrajet from "./components/CaseTrajet.svelte";
   import SearchBar from "./components/SearchBar.svelte";
+    import CreateAccount from './components/CreateAccount.svelte';
 </script>
 
 <body class="bg-red-500 h-screen">
 <header>
   <Header />
+  <CaseTrajet />
+  <SearchBar />
+  <CreateAccount />
+  <LoginForm />
 </header>
 <main>
 </main>
